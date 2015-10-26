d55086382034e4dbef38bf0e8ca3e7f37e060f96
Kael <i@kael.me>
2015-10-26 15:12:35 +0800

    updates test cases

--- test/neuron-package-root.js
+++ test/neuron-package-root.js
@@ -1,4 +1,26 @@
 'use strict';
 
 var expect = require('chai').expect;
-var neuron_package_root = require('../');
+var package_root = require('../');
+var node_path = require('path');
+
+var root = node_path.join(__dirname, 'fixtures');
+var dir = root;
+
+describe("package_root()", function(){
+  [
+    [__dirname],
+    [dir, root],
+    [dir = node_path.join(dir, 'a'), root],
+    [dir = node_path.join(dir, 'b'), root],
+    [dir = node_path.join(dir, 'c'), root]
+  ].forEach(function (c) {
+    it(c[0], function(done){
+      package_root(c[0], function (root) {
+        expect(root).to.equal(c[1]);
+        done();
+      });
+    });
+  });
+});
+
