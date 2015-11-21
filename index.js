fbd25ca51b8ea33cf92c688423c30f94a0961ac0
Kael <i@kael.me>
2015-11-21 13:40:16 +0800

    renames project, moves files

--- index.js
+++ index.js
@@ -1,50 +1,26 @@
 'use strict';
 
-module.exports = package_root;
 
 var node_path = require('path');
 var fs = require('fs');
+var package_root = require('./lib/root');
+var validate = require('./lib/validate');
 
-// Get the root path of the project
-function package_root(dir, options, callback) {
-  if (arguments.length === 2) {
-    callback = options;
-    options = {};
-  }
-
-  options.identity_filename = options.identity_filename || 'neuron.config.js';
-  _package_root(dir, options, callback);
-}
+exports.root = package_root;
+exports.read = read;
+exports.validate = validate;
 
 
-var SYS_ROOT = node_path
-  // in windows, sys_root will be 'c:\\'
-  .resolve('/')
-  // It is weird that sometime the disk name of windows is lowercase, sometime uppercase
-  .toLowerCase();
-
-function _package_root (dir, options, callback) {
-  if (!dir || dir.toLowerCase() === SYS_ROOT) {
-    return callback();
-  }
-
-  var file = node_path.join(dir, options.identity_filename);
-  fs.exists(file, function (exists) {
-    if (exists) {
-      return fs.stat(file, function (err, stat) {
-        if (err) {
-          // undefined
-          return callback();
-        }
-
-        if (stat.isFile()) {
-          return callback(dir);
-        }
-
-        _package_root(node_path.dirname(dir), options, callback);
-      });
+function read (dir, callback) {
+  package_root(dir, function (root) {
+    if (!root) {
+      var message = 'neuron.config.js not found up from "' + dir + '"';
+      return callback(new Error(message));
     }
 
-    _package_root(node_path.dirname(dir), options, callback);
+    var config_file = node_path.join(root, options.identity_filename);
+    var config = require(config_file);
+
+    validate(config, root, callback);
   });
-};
+}
