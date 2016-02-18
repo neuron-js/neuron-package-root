c889804d5c712701b40e8e9dde5b53d4cb8017d6
Kael <i@kael.me>
2016-02-18 15:17:31 +0800

    1.2.0: adds a new method .read_file

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-'use strict';
+'use strict'
 
 
-var node_path = require('path');
-var fs = require('fs');
-var package_root = require('./lib/root');
-var validate = require('./lib/validate');
+var node_path = require('path')
+var fs = require('fs')
+var package_root = require('./lib/root')
+var validate = require('./lib/validate')
 
-exports.root = package_root;
-exports.read = read;
-exports.validate = validate;
+exports.root = package_root
+exports.read = read
+exports.read_file = read_file
+exports.validate = validate
 
 
 function read (dir, callback) {
   package_root(dir, function (root) {
     if (!root) {
-      var message = 'neuron.config.js not found up from "' + dir + '"';
-      return callback(new Error(message));
+      var message = 'neuron.config.js not found up from "' + dir + '"'
+      return callback(new Error(message))
     }
 
-    var config_file = node_path.join(root, package_root.IDENTITY_FILENAME);
-    var config = require(config_file);
+    var config_file = node_path.join(root, package_root.IDENTITY_FILENAME)
+    read_file(config_file, callback)
+  })
+}
 
-    // `config.root` is the root path of the project,
-    // i.e. the directory which contains neuron.config.js
-    config.root = root;
 
-    validate(config, root, callback);
-  });
+function read_file(file, callback) {
+  try {
+    var config = require(file)
+  } catch(e) {
+    e.message = 'could not read config file "' + file + '": ' + e.message
+    return callback(e)
+  }
+
+  var root = node_path.dirname(file)
+
+  // `config.root` is the root path of the project,
+  // i.e. the directory which contains neuron.config.js
+  config.root = root
+
+  validate(config, root, callback)
 }
